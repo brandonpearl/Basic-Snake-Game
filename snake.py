5e44b5dbde992b74ffa09a8b57dd3ab013967dc9
Brandon Pearl <brandonpearl@berkeley.edu>
2016-01-13 00:04:21 -0800

    GUI updates and options

--- snake.py
+++ snake.py
@@ -56,16 +56,20 @@ class SnakeLinks:
 	def getHeadLocation(self):
 		return [self.head.x, self.head.y]
 
+	def getTailLocation(self):
+		return [self.tail.x, self.tail.y]
+
 
 
 class game:
-	def __init__(self, boardSize=15):
+	def __init__(self, boardSizeX=15, boardSizeY=15, safety=False):
 		self.root = Tk()
-		self.size = boardSize
+		self.sizeX = max(boardSizeX, 5)
+		self.sizeY = max(boardSizeY, 5)
 		self.factor = 20
 		self.running = False
 		self.score = 0
-		self.grid = [[False for _ in range(boardSize)] for _ in range(boardSize)]
+		self.grid = [[False for _ in range(self.sizeY)] for _ in range(self.sizeX)]
 		self.snake = SnakeLinks()
 		self.foodX = None
 		self.foodY = None
@@ -74,19 +78,23 @@ class game:
 		self.UP = False
 		self.DOWN = False
 		self.count = 0
-		self.inverseSpeed = 40
+		self.inverseSpeed = 10
 		self.lastMove = ""
+		self.safety = safety
 
 		self.console = Frame(bg="grey")
 		self.console.pack()
 
 		self.scoreBoard = Label(self.console, text="Score: " + str(self.score), fg="black", bg="grey")
-		self.scoreBoard.pack()
+		self.scoreBoard.pack(side="top")
 
-		self.board = Canvas(self.console, bg="black",width=self.factor*self.size, height=self.factor*self.size)
-		self.board.pack()
+		self.board = Canvas(self.console, bg="black",width=self.factor*self.sizeX, height=self.factor*self.sizeY)
+		self.board.pack(side="top")
 
 		self.root.bind("<Key>", self.handler)
+
+		self.entry = Entry(self.console)
+		self.entry.pack(side="top")
 
 		self.startButton = Button(self.console, text="Start", command=self.start, bg="grey", fg="white")
 		self.startButton.pack(side="left")
@@ -94,7 +102,8 @@ class game:
 		self.quitButton = Button(self.console, text="Quit", command=self.root.destroy, bg="grey", fg="white")
 		self.quitButton.pack(side="right")
 
-
+		self.leaveButton = Button(self.console, text="Leave", command=self.runningOff, bg="grey", fg="white")
+		self.leaveButton.pack(side="right")
 
 		self.root.lift()
 		self.root.mainloop()
@@ -137,8 +146,10 @@ class game:
 			self.lastMove = d
 		oldPos = self.snake.getHeadLocation()
 		newPos = [oldPos[0]+moveVector[0], oldPos[1]+moveVector[1]]
-		if newPos[0] >= self.size or newPos[0] < 0 or newPos[1] >= self.size \
-		   or newPos[1] < 0 or self.grid[newPos[0]][newPos[1]]:
+		tailPos = self.snake.getTailLocation()
+		tailEqualNewHead = newPos[0] == tailPos[0] and newPos[1] == tailPos[1]
+		if not tailEqualNewHead and (newPos[0] >= self.sizeX or newPos[0] < 0 or \
+			newPos[1] >= self.sizeY or newPos[1] < 0 or self.grid[newPos[0]][newPos[1]]):
 		    self.running = False
 		elif self.foodX == newPos[0] and self.foodY == newPos[1]:
 			self.score += 1
@@ -149,29 +160,42 @@ class game:
 		else:
 			self.grid[newPos[0]][newPos[1]] = True
 			removePos = self.snake.moveHere(newPos[0], newPos[1])
-			self.grid[removePos[0]][removePos[1]] = False
+			self.grid[removePos[0]][removePos[1]] = False if not tailEqualNewHead else True
 
 
 	def newFood(self):
 		okay = []
-		for x in range(self.size):
-			for y in range(self.size):
+		boundsX = [1, self.sizeX-1] if self.safety else [0, self.sizeX]
+		boundsY = [1, self.sizeY-1] if self.safety else [0, self.sizeY]
+		for x in range(boundsX[0], boundsX[1]):
+			for y in range(boundsY[0], boundsY[1]):
 				if not self.grid[x][y]:
 					okay += [(x, y)]
 		return choice(okay)
 
 	def drawScreen(self):
 		self.board.delete(ALL)
-		for x in range(self.size):
-			for y in range(self.size):
+		for x in range(self.sizeX):
+			for y in range(self.sizeY):
 				if self.grid[x][y]:
 					self.board.create_rectangle(x*self.factor, y*self.factor, (x+1)*self.factor, (y+1)*self.factor, fill="green")
+		x, y = self.snake.getHeadLocation()
+		self.board.create_rectangle(x*self.factor, y*self.factor, (x+1)*self.factor, (y+1)*self.factor, fill="dark green")
 		self.board.create_rectangle(self.foodX*self.factor, self.foodY*self.factor, (self.foodX+1)*self.factor, (self.foodY+1)*self.factor, fill="red")
 
 	def null(self):
 		return
 
 	def start(self):
+		self.board.focus_set()
+		textEntry = self.entry.get()
+		self.entry.delete(0, len(textEntry))
+		try:
+			temp = int(textEntry)
+			if temp > 1 and temp <= 50:
+				self.inverseSpeed = temp
+		except ValueError:
+			pass
 		self.running = True
 		self.LEFT = True
 		self.RIGHT = self.UP = self.DOWN = False
@@ -179,8 +203,8 @@ class game:
 		self.score = 0
 		self.startButton.config(text="Running", command=self.null)
 		self.scoreBoard.config(text="Score: " + str(self.score))
-		self.grid[int(self.size/2)][int(self.size/2)] = True
-		self.snake.moveHead(SnakeLink(int(self.size/2), int(self.size/2)))
+		self.grid[int(self.sizeX/2)][int(self.sizeY/2)] = True
+		self.snake.moveHead(SnakeLink(int(self.sizeX/2), int(self.sizeY/2)))
 		self.foodX, self.foodY = self.newFood()
 		self.run()
 
@@ -193,13 +217,17 @@ class game:
 			self.root.after(5, self.run)
 		else:
 			self.end()
+
+	def runningOff(self):
+		self.running = False
 		
 	def end(self):
 		self.board.delete(ALL)
 		self.snake = SnakeLinks()
-		for x in range(self.size):
-			for y in range(self.size):	
+		self.inverseSpeed = 10
+		for x in range(self.sizeX):
+			for y in range(self.sizeY):	
 				self.grid[x][y] = False
 		self.startButton.config(text="Start", command=self.start)
 		
-snake = game()
+snake = game(40, 20, True)
